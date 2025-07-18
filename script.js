52c3d6e5ca9fcf8997dd85d72d6d50b9d466b5a7
Alpo007 <214224461+Alpo007@users.noreply.github.com>
2025-07-18 18:12:26 +0200

    add 16 divs into each container div
    add 16 drawing divs into each container div with JavaScript

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const container = document.querySelector('.container');
 
 for (let i = 0+1; i <= 16; i++){
     const containerDiv = document.createElement('div');
-    containerDiv.classList.add('containerDiv'+ i);
+    containerDiv.classList.add('containerDiv');
+    containerDiv.classList.add(i)
     container.appendChild(containerDiv);
+
+    for (let a = 0+1; a <= 16; a++){
+        const divDraw = document.createElement('div');
+        divDraw.classList.add('divDraw');
+        divDraw.classList.add(a);
+        containerDiv.appendChild(divDraw);
+}   
 }
+
+/**
+for (let i = 0+1; i <= 16; i++){
+    const divDraw = document.createElement('div');
+    divDraw.classList.add('divDraw');
+    const selectContainerDiv = document.querySelector('.containerDiv'+i);   
+    selectContainerDiv.appendChild(divDraw);
+} **/
