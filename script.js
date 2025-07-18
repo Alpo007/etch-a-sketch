906f5b6eab54032ee9713cbd0e236710c8444ce0
Alpo007 <214224461+Alpo007@users.noreply.github.com>
2025-07-18 18:26:27 +0200

    finetune 16x16 draw box

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const container = document.querySelector('.container');
 for (let i = 0+1; i <= 16; i++){
     const containerDiv = document.createElement('div');
     containerDiv.classList.add('containerDiv');
-    containerDiv.classList.add(i)
+    containerDiv.classList.add('a'+i)
     container.appendChild(containerDiv);
 
     for (let a = 0+1; a <= 16; a++){
         const divDraw = document.createElement('div');
         divDraw.classList.add('divDraw');
-        divDraw.classList.add(a);
+        divDraw.classList.add('a'+a);
         containerDiv.appendChild(divDraw);
 }   
 }
