b7d7713f926cb81c38fcc368583c65f965d30c1c
Alpo007 <214224461+Alpo007@users.noreply.github.com>
2025-07-18 23:20:37 +0200

    add JavaScript to change color of desired div

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let color;
 const container = document.querySelector('.container');
 const green = document.querySelector('#green');
+const yellow = document.querySelector('#yellow');
+const blue = document.querySelector('#blue');
+const red = document.querySelector('#red');
+const black = document.querySelector('#black');
+const eraser = document.querySelector('#eraser');
 
 for (let i = 0+1; i <= 16; i++){
     const containerDiv = document.createElement('div');
@@ -14,3 +20,52 @@ for (let i = 0+1; i <= 16; i++){
         containerDiv.appendChild(divDraw);
 }   
 }
+
+green.addEventListener('click' , function(){
+    color = 'green';
+    console.log(color);
+});
+
+yellow.addEventListener('click' , function(e){
+    color = 'yellow';
+});
+
+blue.addEventListener('click' , function(e){
+    color = 'blue';
+});
+
+red.addEventListener('click' , function(e){
+    color = 'red';
+});
+
+black.addEventListener('click' , function(e){
+    color = 'black';
+});
+
+eraser.addEventListener('click' , function(e){
+    color = 'eraser';
+});
+
+const drawingDivs = document.querySelectorAll('.divDraw');
+let selectedBox;
+
+drawingDivs.forEach((box) => {
+    box.addEventListener('click', () => {
+        if (color === 'green'){
+            box.style.cssText = 'background: green';
+        } else if (color === 'yellow'){
+            box.style.cssText = 'background: yellow'
+        } else if (color === 'blue'){
+            box.style.cssText = 'background: blue'
+        } else if (color === 'red'){
+            box.style.cssText = 'background: red'
+        } else if (color === 'black'){
+            box.style.cssText = 'background: black'
+        } else if (color === 'eraser'){
+            box.style.cssText = 'background: white'
+        }
+    });
+});
+
+
+
